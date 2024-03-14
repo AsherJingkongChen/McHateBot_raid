9573ec37fe00e53d43fa2080527e21f760c03061
AsherJingkongChen <37398747+AsherJingkongChen@users.noreply.github.com>
2024-03-15 00:29:49 +0800

    feat: Includes Linux as a build target

--- scripts/build-nix.sh
+++ scripts/build-nix.sh
@@ -10,10 +10,11 @@ cp settings-orginal.json out
 mkdir -p out && cp -r dist out
 mv out/config-orginal.json out/config.json
 mv out/settings-orginal.json out/settings.json
-npm exec -- pkg ./out/McHateBot.js -t node18-macos-x64 -c package.json
+npm exec -- pkg ./out/McHateBot.js -t node18 -c package.json
 cp config-orginal.json exe
 cp settings-orginal.json exe
 mkdir -p exe && cp -r language exe
 cp .env exe
 mv exe/config-orginal.json exe/config.json
 mv exe/settings-orginal.json exe/settings.json
+mv exe/mchatebot_raid exe/mchatebot_raid.exe
